40008f773d1b44c6d8a65f1f6d17072ab220dc44
Sourabh Sinha <sourabhsinha396@gmail.com>
2025-07-19 18:09:13 +0530

    add support for alembic

--- database/db.py
+++ database/db.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import create_engine, Session
 from core.config import settings
 
 print(f"DATABASE_URL is: {settings.DATABASE_URL}")
