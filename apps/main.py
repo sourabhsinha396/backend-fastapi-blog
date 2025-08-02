57ced189cc10afb22203db92edfa57bf3fd9c745
Sourabh Sinha <sourabhsinha396@gmail.com>
2025-08-02 21:53:27 +0530

    refactor, blog-list , navbar

--- apps/main.py
+++ apps/main.py
@@ -4,4 +4,4 @@ from apps.v1.blog import router as blog_router
 
 app_router = APIRouter()
 
-app_router.include_router(blog_router, prefix="/blogs", tags=["blogs app"])
+app_router.include_router(blog_router, prefix="/blogs", tags=["blogs app"]) # include_in_schema=False to remove the endpoint from the swagger ui
